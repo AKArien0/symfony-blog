04a7aafdf0e9f062eadffe1cfed14e8f81a2b4df
Hue <skeptikal.monke@gmail.com>
2024-11-29 11:33:05 +0100

    added a form to create categories. almost went unnoticed. oops !

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+
+use Doctrine\ORM\EntityManagerInterface;
 
 class CategoryController extends AbstractController
 {
@@ -19,5 +23,27 @@ class CategoryController extends AbstractController
 		}
 		$posts = $category->getPosts();
         return $this->render('category/overview.html.twig', ["category" => $category, "posts" => $posts]);
+    }
+
+    #[IsGranted('ROLE_ADMIN')]
+	#[Route('/create/category', name: 'create_category')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Category created successfully.');
+
+            return $this->redirectToRoute('create_post');
+        }
+
+        return $this->render('category/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
